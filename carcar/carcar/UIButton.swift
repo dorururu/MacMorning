89484ce426fff4f95374de2543eea04ee26df9cc
KM Lee <petibelle29@naver.com>
2018-05-18 14:46:45 +0900

    main page UI

---
+++ carcar/carcar/UIButton.swift
@@ -0,0 +1,33 @@
+//
+//  UIButton.swift
+//  carcar
+//
+//  Created by KMLee on 2018. 5. 15..
+//  Copyright © 2018년 MacMorning. All rights reserved.
+//
+
+import UIKit
+
+class UIRoundButton1: UIButton {
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)!
+        self.layer.cornerRadius = 5.0;
+        self.backgroundColor = UIColor(red: 136/255, green: 199/255, blue: 193/255, alpha: 1)
+    }
+}
+
+class UIRoundButton2: UIButton {
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)!
+        self.layer.cornerRadius = 5.0;
+        self.backgroundColor = UIColor(red: 100/255, green: 113/255, blue: 141/255, alpha: 1)
+    }
+}
+
+class UIRoundButton3: UIButton {
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)!
+        self.layer.cornerRadius = 5.0;
+        self.backgroundColor = UIColor(red: 197/255, green: 162/255, blue: 219/255, alpha: 1)
+    }
+}
---
+++ carcar/carcar/ViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController.swift
+//  carcar
+//
+//  Created by KMLee on 2018. 5. 15..
+//  Copyright © 2018년 MacMorning. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var homeImage: UIImageView!
+    
+    func showImage() {
+        homeImage.image = UIImage(named: "home.png")
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        showImage()
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+}
