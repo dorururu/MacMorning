65b2b39954bb293349a8f6bbebb3de510639d96b
혜원 <hyewon@hyewon-ui-macbook-pro.local>
2018-06-01 00:24:49 +0900

    DataController.swift - 디비 정보 불러옴 + 차 이미지 보여주기

--- carcar/carcar/DataController.swift
+++ carcar/carcar/DataController.swift
@@ -11,21 +11,20 @@ import UIKit
 class DataController: UIViewController {
 
     @IBOutlet weak var dataText: UITextView!
-    
+    @IBOutlet weak var carinfoImage: UIImageView!
     var carName=""
+    var carImage: UIImage!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        dataText.text = carName
-        
-        //self.loadData()
+        showImage(carName)
+        self.loadData(carName)
         // Do any additional setup after loading the view, typically from a nib.
     }
     
     func loadData(_ text: String) {
         
         dataText.isEditable = false
-        //dataText.insertText(text)
         
         let fileManager = FileManager.default
         let dirPaths = fileManager.urls(for: .documentDirectory, in: .userDomainMask)[0]
@@ -54,7 +53,7 @@ class DataController: UIViewController {
         
             if carDB.open() {
                 NSLog("디비 열림")
-                let sql = "SELECT name, info FROM car WHERE name = 'kia'"
+                let sql = "SELECT * FROM car WHERE name = '\(carName)'"
                 let paramDictionary = [NSObject:AnyObject]()
                 let results:FMResultSet? = carDB.executeQuery(sql, withParameterDictionary: paramDictionary)
             
@@ -62,16 +61,58 @@ class DataController: UIViewController {
                     print("Error: \(carDB.lastErrorMessage())")
                 }else{
                     while(results?.next() == true) {
-                        NSLog((results?.string(forColumn: "name"))!)
-                        NSLog((results?.string(forColumn: "info"))!)
-                        dataText.insertText((results?.string(forColumn: "name"))!)
-                        dataText.insertText((results?.string(forColumn: "info"))!)
+                        dataText.insertText((results?.string(forColumn: "name"))!+"\n")
+                        dataText.insertText("모델명 : "+(results?.string(forColumn: "model"))!+"\n")
+                        dataText.insertText("배기량 : "+(results?.string(forColumn: "displacement"))!+"\n")
+                        dataText.insertText("최대출력 : "+(results?.string(forColumn: "maxOutput"))!+"\n")
+                        dataText.insertText("연비 : "+(results?.string(forColumn: "efficiency"))!+"\n")
+                        dataText.insertText("출시가 : "+(results?.string(forColumn: "price"))!+"\n")
                     }
                 }
             }else {
                 NSLog("디비 안 열림")
             }
         }
+    }
+    
+    func showImage(_ carName: String) {
+        switch carName {
+        case "benz_eclass":
+            carinfoImage.image = UIImage(named: "benz_eclass.png")
+        case "benz_gls":
+            carImage = UIImage(named: "benz_gls.png")
+        case "audi_a6":
+            carImage = UIImage(named: "audi_a6.png")
+        case "audi_q7":
+            carImage = UIImage(named: "audi_q7.png")
+        case "bmw_i3":
+            carImage = UIImage(named: "bmw_i3.png")
+        case "bmw_i8":
+            carImage = UIImage(named: "bmw_i8.png")
+        case "hyundai_kona":
+            carImage = UIImage(named: "hyundai_kona.png")
+        case "hyundai_veloster":
+            carImage = UIImage(named: "hyundai_veloster.png")
+        case "kia_k9":
+            carImage = UIImage(named: "kia_k9.png")
+        case "kia_allnewmorning":
+            carImage = UIImage(named: "kia_allnewmorning.png")
+        case "toyota_sienna":
+            carImage = UIImage(named: "toyota_sienna.png")
+        case "toyota_camry":
+            carImage = UIImage(named: "toyota_camry.png")
+        case "volkswagen_tiguan":
+            carImage = UIImage(named: "volkswagen_tiguan.png")
+        case "volkswagen_passat":
+            carImage = UIImage(named: "volkswagen_passat.png")
+        case "mini_countryman":
+            carImage = UIImage(named: "mini_countryman.png")
+        case "mini_hatch":
+            carImage = UIImage(named: "mini_hatch.png")
+        default:
+            NSLog("error")
+        }
+        carinfoImage.image = carImage
     }
     
     
